edd50d5a27f0832ab25bbe68550371147eb396db
ashish1ankit <32021646+ashish1ankit@users.noreply.github.com>
2026-03-18 00:12:04 +0530

    added logs

--- src/main/java/com/zachlop/checkom/checkpol/controller/PramController.java
+++ src/main/java/com/zachlop/checkom/checkpol/controller/PramController.java
@@ -2,12 +2,15 @@ package com.zachlop.checkom.checkpol.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class PramController {
-
+private static final Logger logger = LoggerFactory.getLogger(PramController.class);
     @GetMapping("/check")
     public String checkStatus(){
+        logger.info("🟢 SUCCESS: Someone just visited the /hello endpoint!");
         return "Application is up";
     }
 }
